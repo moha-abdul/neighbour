1d1b5257e291bad6a3dc3c6d0b5c25cfe09f8d22
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-10-20 21:38:26 +0300

    Add view function for signup

--- neighbour_app/views.py
+++ neighbour_app/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.http import HttpResponse,Http404
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from .forms import SignupForm,ProfileForm,ProjectForm
+from django.contrib.auth.models import User
+from .models import Profile, Project
+from django.core.mail import EmailMessage
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            profile=Profile(user=user)
+            profile.save()             
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your awwwwwwwwwwww account.'
+            message = render_to_string('registration/acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
