80cdd9fe981fd8e0fe6b3737dcf64ec6a08cfaad
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-10-20 21:48:21 +0300

    Set url paths

---
+++ neighbour_app/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns=[
+
+    url(r'^$',views.home,name = 'home'),
+    url(r'signup/', views.signup, name='signup'),
+    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
+    url(r'^profile/',views.profile,name='profile'),
+    url(r'^edit_profile/', views.edit_profile, name = 'edit_profile'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- neighbour_project/urls.py
+++ neighbour_project/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views 
 
 urlpatterns = [
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'', include('awwwards.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
